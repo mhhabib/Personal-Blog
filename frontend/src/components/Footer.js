54debd1afd0aef65475385ee88b07f9ec221ef68
mhhabib <mhrahman7096@gmail.com>
2024-01-22 21:12:41 +0600

    Added hiddn login button

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,25 @@
 "use client";
 import { Footer } from "flowbite-react";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Footerbar = () => {
 	const { username, logoutUser } = useContext(AuthContext);
+	const [count, setCount] = useState(0)
+	const navigate = useNavigate()
+
+	const handleLoginPage = () =>{
+		setTimeout(() => {
+			setCount(count+1)
+			if(count>5){
+				console.log("Count: ", count)
+				setCount(0);
+				navigate('/login')
+			}
+		}, 1000);
+	}
 	return (
 		<div className="pt-10 bg-gray-100">
 			<Footer bgDark>
@@ -52,6 +66,7 @@ const Footerbar = () => {
 								by="Lensoflife"
 								href="/"
 								year={2022}
+								onClick={handleLoginPage}
 							/>
 							<div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
 								<Footer.Icon href="#" icon={BsFacebook} />
