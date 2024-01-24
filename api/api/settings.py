9dd36f1fc8be51b8e5d80c2eaef98ea8722d023e
mhhabib <mhrahman7096@gmail.com>
2024-01-24 22:19:19 +0600

    Added dotenv to hide security key

--- api/api/settings.py
+++ api/api/settings.py
@@ -14,6 +14,10 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&m0!e3_9*z+0d1q!dtbxe*0rvcmcix-&(k7=grt27316@*d8e7"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
